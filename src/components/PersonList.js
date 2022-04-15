dbbdbc23f1f6103eb9483d01415e9ac199089a65
Moni <moniruzzaman.eng1995@gmail.com>
2022-04-15 14:55:51 +0300

    Fixed typo error

---
+++ src/components/PersonList.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import axios from 'axios';
+import './style.css';
+
+export default class PersonList extends React.Component {
+    state = {
+        persons: []
+    };
+
+    componentDidMount() {
+        axios.get(`https://jsonplaceholder.typicode.com/users`).then((res) => {
+            const persons = res.data;
+            this.setState({ persons });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <main className='deals'>
+                    {this.state.persons.map((person) => (
+                        <article className='sale-item' key={person.id}>
+                            <h1 className='initialLetter'> {person.name.charAt(0)}</h1>
+                            <p>{person.name}</p>
+                            <p> {person.username}</p>
+                            <p>
+                                {' '}
+                                <a href='{person.website}'>{person.website} </a>
+                            </p>
+                            <button style={{ color: 'blue' }}>View Details</button>
+                        </article>
+                    ))}
+                </main>
+            </div>
+        );
+    }
+}
